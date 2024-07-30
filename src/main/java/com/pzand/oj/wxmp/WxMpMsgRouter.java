8d3f1f896491b62cdaa79bbe07114a04daebc861
‘pzand’ <pzand17875626301@163.com>
2024-07-30 22:09:54 +0800

    SpringBoot初始模板

---
+++ src/main/java/com/pzand/oj/wxmp/WxMpMsgRouter.java
@@ -0,0 +1,61 @@
+package com.pzand.oj.wxmp;
+
+import com.pzand.oj.wxmp.handler.EventHandler;
+import com.pzand.oj.wxmp.handler.SubscribeHandler;
+import com.pzand.oj.wxmp.handler.MessageHandler;
+import jakarta.annotation.Resource;
+import me.chanjar.weixin.common.api.WxConsts.EventType;
+import me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 微信公众号路由
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Configuration
+public class WxMpMsgRouter {
+
+    @Resource
+    private WxMpService wxMpService;
+
+    @Resource
+    private EventHandler eventHandler;
+
+    @Resource
+    private MessageHandler messageHandler;
+
+    @Resource
+    private SubscribeHandler subscribeHandler;
+
+    @Bean
+    public WxMpMessageRouter getWxMsgRouter() {
+        WxMpMessageRouter router = new WxMpMessageRouter(wxMpService);
+        // 消息
+        router.rule()
+                .async(false)
+                .msgType(XmlMsgType.TEXT)
+                .handler(messageHandler)
+                .end();
+        // 关注
+        router.rule()
+                .async(false)
+                .msgType(XmlMsgType.EVENT)
+                .event(EventType.SUBSCRIBE)
+                .handler(subscribeHandler)
+                .end();
+        // 点击按钮
+        router.rule()
+                .async(false)
+                .msgType(XmlMsgType.EVENT)
+                .event(EventType.CLICK)
+                .eventKey(WxMpConstant.CLICK_MENU_KEY)
+                .handler(eventHandler)
+                .end();
+        return router;
+    }
+}
